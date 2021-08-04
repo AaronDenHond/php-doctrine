8ec94c3a9f28a4e488ce7a9257ba34db652cad39
Aaron Den Hond <aaron.denhond@gmail.com>
2021-08-04 14:37:19 +0200

    made 2 entities with props, 3rd class as an embedded class in both student and teacher

---
+++ src/Entity/Address.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Entity;
+
+class Address {
+
+    private $address;
+
+
+public function __construct()
+{
+    $this->address = new Address();
+}
+
+}
---
+++ src/Entity/Student.php
@@ -0,0 +1,114 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\StudentRepository;
+use Doctrine\ORM\Mapping as ORM;
+use App\Entity\Address;
+
+/**
+ * @ORM\Entity(repositoryClass=StudentRepository::class)
+ */
+class Student
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=20)
+     */
+    private $firstName;
+
+    /**
+     * @ORM\Column(type="string", length=20)
+     */
+    private $lastName;
+
+    /**
+     * @ORM\Column(type="string", length=40)
+     */
+    private $email;
+
+     /** @Embedded(class = "Address") */
+
+    private $address;
+
+    /**
+     * @ORM\ManyToOne(targetEntity=Teacher::class, inversedBy="students")
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $teacher;
+
+    
+   
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getFirstName(): ?string
+    {
+        return $this->firstName;
+    }
+
+    public function setFirstName(string $firstName): self
+    {
+        $this->firstName = $firstName;
+
+        return $this;
+    }
+
+    public function getLastName(): ?string
+    {
+        return $this->lastName;
+    }
+
+    public function setLastName(string $lastName): self
+    {
+        $this->lastName = $lastName;
+
+        return $this;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): self
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    public function getAddress(): ?string
+    {
+        return $this->address;
+    }
+
+    public function setAddress(string $address): self
+    {
+        $this->address = $address;
+
+        return $this;
+    }
+
+    public function getTeacher(): ?Teacher
+    {
+        return $this->teacher;
+    }
+
+    public function setTeacher(?Teacher $teacher): self
+    {
+        $this->teacher = $teacher;
+
+        return $this;
+    }
+
+}
